fb5722be83dd336d1a704c266edd69ec1fbab203
Edouard Demotes <e.demotes@gmail.com>
2019-03-06 16:02:22 -0500

    Refactor again

--- src/services/generators/Generator.ts
+++ src/services/generators/Generator.ts
@@ -1,5 +1,5 @@
 import { Service } from 'typedi';
-import { TemplateInput, ITemplate, IModel, IField, IGeneratorResult, Access, FieldType, NumberSubType, TemplateEngine } from '../interfaces';
+import { TemplateInput, ITemplate, IModel, IField, IGeneratorResult, Access, FieldType, FieldSubType, TemplateEngine } from '../interfaces';
 import { JavaScriptGeneratorService, HpfGeneratorService } from './';
 import { StringService } from '../String';
 
@@ -276,11 +276,11 @@ export class GeneratorService {
 			mainlyHidden: fields.length < 2 * hidden.length,
 			mainlyInternal: fields.length < 2 * internal.length,
 			isGeolocated:
-				fields.filter((f: IField) => f.type === FieldType.Number && f.subtype === NumberSubType.Latitude).length > 0 &&
-				fields.filter((f: IField) => f.type === FieldType.Number && f.subtype === NumberSubType.Longitude).length > 0,
+				fields.filter((f: IField) => f.type === FieldType.Number && f.subtype === FieldSubType.Number.Latitude).length > 0 &&
+				fields.filter((f: IField) => f.type === FieldType.Number && f.subtype === FieldSubType.Number.Longitude).length > 0,
 			isGeoSearchable:
-				fields.filter((f: IField) => f.type === FieldType.Number && f.subtype === NumberSubType.Latitude && f.searchable).length > 0 &&
-				fields.filter((f: IField) => f.type === FieldType.Number && f.subtype === NumberSubType.Longitude && f.searchable).length > 0
+				fields.filter((f: IField) => f.type === FieldType.Number && f.subtype === FieldSubType.Number.Latitude && f.searchable).length > 0 &&
+				fields.filter((f: IField) => f.type === FieldType.Number && f.subtype === FieldSubType.Number.Longitude && f.searchable).length > 0
 		};
 
 		// ==========================================
--- src/services/interfaces/FieldType.ts
+++ src/services/interfaces/FieldType.ts
@@ -5,9 +5,24 @@ export class FieldType {
 	static DateTime = 'datetime';
 	static Entity = 'entity';
 }
-export class NumberSubType {
-	static Integer = 'integer';
-	static Float = 'float';
-	static Latitude = 'latitude';
-	static Longitude = 'longitude';
+
+export class FieldSubType {
+	static Boolean = {};
+	static Number = {
+		Integer: 'integer',
+		Float: 'float',
+		Latitude: 'latitude',
+		Longitude: 'longitude'
+	};
+	static String = {
+		Email: 'email',
+		Password: 'password',
+		Text: 'text',
+		RichText: 'rich'
+	};
+	static DateTime = {
+		Date: 'date',
+		Time: 'time'
+	};
+	static Entity = {};
 }
