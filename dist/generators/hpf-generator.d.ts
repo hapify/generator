17532c62197f28325cfef68943c5d00ca57d6d85
Edouard Demotes-Mainard <e.demotes@gmail.com>
2020-11-14 11:32:46 -0500

    build: increase version and build

--- dist/generators/hpf-generator.d.ts
+++ dist/generators/hpf-generator.d.ts
@@ -3,12 +3,4 @@ export declare class HpfGenerator implements GeneratorWorker {
     constructor();
     one(model: ExplicitModel, template: Template): Promise<string>;
     all(models: ExplicitModel[], template: Template): Promise<string>;
-    /**
-     * Cleanup code before process
-     */
-    private preProcess;
-    /**
-     * Cleanup code after process
-     */
-    private postProcess;
 }
