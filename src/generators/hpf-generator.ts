6bb82fc8ca6dbb464cdf8b4c84475b44d8f11aff
Edouard Demotes-Mainard <e.demotes@gmail.com>
2020-11-14 13:31:41 -0500

    feat(errors): append file name for evaluation errors

--- src/generators/hpf-generator.ts
+++ src/generators/hpf-generator.ts
@@ -1,6 +1,7 @@
 import { HapifySyntax } from '@hapify/syntax';
 import { Config } from '../config';
 import { ExplicitModel, GeneratorWorker, Template } from '../interfaces';
+import { EvaluationError } from '../errors';
 
 const HpfOptions = { timeout: Config.Generator.timeout };
 
@@ -8,10 +9,27 @@ export class HpfGenerator implements GeneratorWorker {
 	constructor() {}
 
 	async one(model: ExplicitModel, template: Template): Promise<string> {
-		return HapifySyntax.run(template.content, model, HpfOptions);
+		try {
+			return HapifySyntax.run(template.content, model, HpfOptions);
+		} catch (error) {
+			throw this.appendFileName(error, template);
+		}
 	}
 
 	async all(models: ExplicitModel[], template: Template): Promise<string> {
-		return HapifySyntax.run(template.content, models, HpfOptions);
+		try {
+			return HapifySyntax.run(template.content, models, HpfOptions);
+		} catch (error) {
+			throw this.appendFileName(error, template);
+		}
+	}
+
+	/** Append file name to error details if applicable */
+	protected appendFileName(error: Error, template: Template): Error {
+		if (typeof (<EvaluationError>error).lineNumber !== 'undefined') {
+			// Append file name
+			(<EvaluationError>error).details += `, File: ${template.path}`;
+		}
+		return error;
 	}
 }
